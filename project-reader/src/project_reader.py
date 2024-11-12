bd26542c650fbc7bf6f5aa7ca3b80fe9e3705fdc
Vellemu <veetie@hotmail.fi>
2024-11-12 18:13:06 +0200

    Poetry-web sekä project_reader tehtävät

--- project-reader/src/project_reader.py
+++ project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomllib as toml
 
 
 class ProjectReader:
@@ -9,7 +10,8 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
+        content = toml.loads(content)
         print(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project("Ohtutesting app", "Sovellus joka toimii testisyötteenä ohtun osan 2 laskareihin", "MIT", "\n- Matti Luukkainen \n- Kalle Ilves", ["\n- python\n- Flask\n- editdistance"], ["\n- coverage\n- robotframework\n- robotframework-seleniumlibrary\n- requests"])
